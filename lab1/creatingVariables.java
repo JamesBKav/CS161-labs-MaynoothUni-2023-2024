02db26e11255e3375d6ce4d5cdd98f390ed500cf
James <90049146+Schlingel15@users.noreply.github.com>
2024-05-21 16:16:16 +0100

    Create creatingVariables.java

---
+++ lab1/creatingVariables.java
@@ -0,0 +1,12 @@
+public class creatingVariables
+{
+    public static void main(String[]args)
+    {
+        int num1 = 12;
+        double num2 = 123.4;
+        char letter = 'c';
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(letter);
+    }
+} 
