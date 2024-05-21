7913e46869134897459e3883152c9b90e952066c
James <90049146+Schlingel15@users.noreply.github.com>
2024-05-21 16:23:50 +0100

    Create UpDown.java

---
+++ UpDown.java
@@ -0,0 +1,19 @@
+public class UpDown
+{
+    public static void main (String [] args)
+    {
+        int j = 10;
+        while (j <= 20)
+        {
+            System.out.print(j+" ");
+            j++;
+        }
+
+        System.out.println();
+
+        for (int i=20; i <= 10; i--)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
