13c34e4a380da9d4643b4d4f5add5362c1a06c5c
James <90049146+Schlingel15@users.noreply.github.com>
2024-05-21 16:14:25 +0100

    Create theName.java

---
+++ lab1/theName.java
@@ -0,0 +1,9 @@
+public class theName
+{
+    public static void main(String[]args)
+    {
+        System.out.print("John Doe");
+        System.out.println("John Doe");
+        System.out.println("John Doe");
+    }
+}
