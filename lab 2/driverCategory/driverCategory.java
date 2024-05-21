6681ca66c76ba58ba2bdea7046b7ca3d5e61f390
James <90049146+Schlingel15@users.noreply.github.com>
2024-05-21 16:20:11 +0100

    Create driverCategory.java

---
+++ lab 2/driverCategory/driverCategory.java
@@ -0,0 +1,25 @@
+public class driverCategory
+{
+    public static void main(String[]args)
+    {
+        char license = 'a';
+        
+        if (license == 'a')
+        {
+            System.out.println("Motorcycle");
+        }
+        else if (license == 'b')
+        {
+            System.out.println("Car");
+        }
+        else if (license == 'c')
+        {
+            System.out.println("Truck");
+        }
+        else if (license == 'd')
+        {
+            System.out.println("Bus");
+        }
+    }
+}
+
