c5901b4e7d499e91ad73461bf8153b49e59da890
James <90049146+Schlingel15@users.noreply.github.com>
2024-05-21 16:17:25 +0100

    Create retirement.java

---
+++ lab 2/retirement.java
@@ -0,0 +1,16 @@
+public class retirement
+{
+    public static void main(String[]args)
+    {
+        int age = 66;
+
+        if (age >= 66) 
+        {
+            System.out.println("Person is retired");
+        }
+        else if (age < 66)
+        {
+            System.out.println("Person is not retired");
+        }
+    }
+}
